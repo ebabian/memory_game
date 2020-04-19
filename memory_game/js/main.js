9145957d161c1307bb7330d496fb322fdc57f0cb
ebabian <40578238+ebabian@users.noreply.github.com>
2020-04-19 19:36:15 -0400

    Attempted to execute flip card

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,9 +11,30 @@
 /*let cardOne;
 let cardTwo;*/
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = [];
-
+const src = document.setAtrribute("src", cards[cardId].cardImage);
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
@@ -30,7 +51,9 @@ cards[2] = cardTwo;
 cardsInPlay.push('cardTwo');
 console.log("User flipped king");*/
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+
 if (cardsInPlay.length === 2) {
 	console.log("There are 2 cards");
 } else if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -39,9 +62,24 @@ if (cardsInPlay.length === 2) {
 	alert("Sorry, try again.");
 	
 } 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
-flipCard(0);
-flipCard(2);
 checkForMatch();
+
+function createBoard() {
+for (let i = 0; i < cards.length; i++) {
+	let cardElement = document.createElement('img');
+	cardElement.setAtrribute('src', 'images/back.png');
+	cardElement.setAtrribute('data-id', i);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+} 
+document.getElementByTagName('cardElement').addEventListener('click', flipCard);
+createBoard();
+
+
+
+
