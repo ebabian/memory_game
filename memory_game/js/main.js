9921ddbf76cb164f7394414794ee3aafff420402
ebabian <40578238+ebabian@users.noreply.github.com>
2020-04-27 09:37:56 -0400

    Trying to display cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,70 +1,69 @@
 
 const cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-diamonds.png"
-}
-]
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{	
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 let cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay.length === 2) {
+function checkForMatch() 
+{
+	//if (cardsInPlay.length === 2) {
 		//console.log("There are 2 cards");
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");
-	} else {
-  		console.log("Sorry, try again.");
-}
-}
-function flipCard() {
-	
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  			console.log("You found a match!");
+		} else {
+  			console.log("Sorry, try again.");
+		}
+	}
+//}
+function flipCard() 
+{	
 	let cardId = this.getAttribute('data-id');
 	this.setAtrribute('src', cards[cardId].cardImage);
-
-// if (cardsInPlay.length === 2) {
-// 	console.log("There are 2 cards");
-// } else if (cardsInPlay[0] === cardsInPlay[1]) {
-// 	alert("You found a match!");
-// } else {
-// 	alert("Sorry, try again.");
-	
-// } 
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
-	
-	checkForMatch();
+	if (cardsInPlay.length === 2) {
+		//console.log("There are 2 cards");
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  			alert("You found a match!");
+		} else {
+  			alert("Sorry, try again.");
+		}
+		checkForMatch();
+	}
+
 }
 
-
-function createBoard() 
-
-{	
-	for (let i = 0; i < cards.length; i++) 
-{
-	let cardElement = document.createElement('img');
-	cardElement.setAtrribute('src', "images/back.png");
-	cardElement.setAtrribute('data-id', i);
-	cardElement.addEventListener('click', flipCard);
-	document.getElementById('game-board').appendChild(cardElement);
-} 
+function createBoard() {	
+	for (let i = 0; i < cards.length; i++) {
+		
+		let cardElement = document.createElement('img');
+		cardElement.setAtrribute('src', "images/back.png");
+		cardElement.setAtrribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	} 
 } 
 createBoard();
